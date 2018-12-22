315f3dcd7c272da0ba388bad4f7136461eadd6e0
yimingnzhao <90603yahoo@gmail.com>
2018-12-21 16:58:01 -0800

    added redirects

--- server.js
+++ server.js
@@ -8,18 +8,13 @@ function handleRoot(req, res) {
     res.sendFile(path.join(__dirname + '/frontend/home.html'));
 };
 
-function handleHello(req, res) {
-    res.send('Hello')
+function handleClass(req, res) {
+    res.sendFile(path.join(__dirname + '/frontend/classList.html'));
 }
 
 app.get('/', handleRoot);
 
-app.get('/hello', handleHello);
+app.get('/class-list', handleClass);
 
-app.get('/hitme', function(req, res) {
-    console.log("Hello!");
-    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
-    res.send("Success");
-});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}...`))
