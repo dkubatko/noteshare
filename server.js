911698d47a894c2e27c98a0210988d3e69e80c13
yimingnzhao <90603yahoo@gmail.com>
2018-12-21 16:13:33 -0800

    set up basic front/back end

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 var path = require('path')
 const app = express()
 const port = 3000
+app.use(express.static(__dirname + '/frontend/'))
 
 function handleRoot(req, res) {
     res.sendFile(path.join(__dirname + '/frontend/home.html'));
