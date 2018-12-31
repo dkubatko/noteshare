606598d332882c19b22b4fca8e5864f16fa5d670
yimingnzhao <90603yahoo@gmail.com>
2018-12-31 01:35:29 -0800

    added search function

--- frontend/static/js/classList.js
+++ frontend/static/js/classList.js
@@ -1,4 +1,27 @@
 $(document).ready(function() {
+    let head = document.getElementById('header');
+    let courses = document.querySelectorAll('.course:not(#header)');
+    hideCourses(head, courses);
+
+    $('#input').on('input', function() {
+        if ($(this).val()=='') {
+            hideCourses(head, courses);
+            return;
+        }
+        let count = 0;
+        let value = $(this).val().toUpperCase();
+        courses.forEach( function(course) {
+            let a = course.querySelector('.course-no').getElementsByTagName('a')[0];
+            let txtValue = a.innerText || a.textContent; // returns A if A is true, else B
+            if (txtValue.toUpperCase().indexOf(value) > -1) {
+                course.style.display = 'flex';
+                count+=1;
+            } else {
+                course.style.display = 'none';
+            }
+        });
+        head.style.display = (count > 0) ? 'flex' : 'none';
+    });
     $('#back').click( function() {
         document.location.href = '/';
     });
@@ -6,3 +29,14 @@ $(document).ready(function() {
         document.location.href = '/add-course';
     });
 });
+
+/**
+ * @param {element} head
+ * @param {array} courses 
+ */
+function hideCourses(head, courses) {
+    head.style.display = 'none';
+    courses.forEach( function(course) {
+        course.style.display = 'none';
+    });
+}
