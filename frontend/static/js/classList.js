e462b638e9e05ad2fb317812203b57470f47a92f
yimingnzhao <90603yahoo@gmail.com>
2019-01-17 11:23:16 -0800

    made form

--- frontend/static/js/classList.js
+++ frontend/static/js/classList.js
@@ -1,16 +1,42 @@
+let results = {};
+let added_data = false;
+
 $(document).ready(function() {
-    let head = document.getElementById('header');
-    let courses = document.querySelectorAll('.course:not(#header)');
-    hideCourses(head, courses);
+    $.ajax({
+        type: "GET",
+        url: "http://127.0.0.1:3000/get-data",
+        success: function(result) {
+            results = result; 
+        }  
+    });
 
     $('#input').on('input', function() {
+        if (!added_data) {
+            for (let i=0; i<results['class_no'].length; i+=1) {
+                let code = '<div class="course">' + 
+                           '<div class="course-no">' + 
+                           '<a href="#">' + results['class_no'][i] + '</a>' + 
+                           '</div>' + 
+                           '<div class="course-name">' + 
+                           '<a href="#">' + results['class_name'][i] + '</a>' +
+                           '</div>' + 
+                           '<div class="note-count">' +
+                           '<a href="#">' + results['note_count'][i] + '</a>' + 
+                           '</div>' + 
+                           '</div>';
+                $('#courses').append(code);
+            }
+            added_data = true;
+            hideCourses($('#header')[0], $('.course:not(#header)'));
+        }
+/*
         if ($(this).val()=='') {
-            hideCourses(head, courses);
+            hideCourses($('#header')[0], $('.course:not(#header)'));
             return;
         }
         let count = 0;
         let value = $(this).val().toUpperCase();
-        courses.forEach( function(course) {
+        $('.course:not(#header)').each( function(course) {
             let a = course.querySelector('.course-no').getElementsByTagName('a')[0];
             let txtValue = a.innerText || a.textContent; // returns A if A is true, else B
             if (txtValue.toUpperCase().indexOf(value) > -1) {
@@ -20,7 +46,8 @@ $(document).ready(function() {
                 course.style.display = 'none';
             }
         });
-        head.style.display = (count > 0) ? 'flex' : 'none';
+        $('#header')[0].style.display = (count > 0) ? 'flex' : 'none';
+        */
     });
     $('#back').click( function() {
         document.location.href = '/';
@@ -28,7 +55,10 @@ $(document).ready(function() {
     $('#add').click( function() {
         document.location.href = '/add-course';
     });
+
 });
+
+
 
 /**
  * @param {element} head
@@ -36,7 +66,7 @@ $(document).ready(function() {
  */
 function hideCourses(head, courses) {
     head.style.display = 'none';
-    courses.forEach( function(course) {
+    courses.each( function(course) {
         course.style.display = 'none';
     });
 }
--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const fs = require('fs');
+const qs = require('querystring');
 var path = require('path');
 const app = express();
 const port = 3000;
+
+var classNo = ["CSE 11", "CSE 12"];
+var className = ["Intro to CS", "Algorithms"];
+var noteCount = [1, 2];
+
 app.use(express.static(__dirname + '/frontend/'));
 
 function handleRoot(req, res) {
@@ -13,15 +19,58 @@ function handleClass(req, res) {
     res.sendFile(path.join(__dirname + '/frontend/classList.html'));
 };
 
-function handleAdd( req, res) {
+function handleAdd(req, res) {
     res.sendFile(path.join(__dirname + '/frontend/addClass.html'));
 }
+
+function handleCourses(req, res) {
+    var data = ["cse100", "bild12"];
+    res.send(data);
+}
+
+function handleAddedClass(req, res) {
+    if (req.method == 'POST') {
+        var body = '';
+
+        req.on('data', function(data) {
+            body += data;
+            // Too much POST data, kill the connection!
+            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+            if (body.length > 1e6)
+                req.connection.destroy();
+        });
+
+        req.on('end', function() {
+            var post = qs.parse(body);
+            classNo.push(post.class_no);
+            className.push(post.class_name);
+            res.redirect('/class-list');
+        });
+    }
+}
+
+function handleGetData(req, res) {
+    let data = {};
+    data['class_no'] = classNo;
+    data['class_name'] = className;
+    data['note_count'] = noteCount;
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.send(data);
+}
+
+
 
 app.get('/', handleRoot);
 
 app.get('/class-list', handleClass);
 
 app.get('/add-course', handleAdd);
+
+app.post('/added-class', handleAddedClass);
+
+app.get('/courses', handleCourses);
+
+app.get('/get-data', handleGetData);
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}...`));
